7fa602569adeae6c4530b91767ba8d6cf78ae46b
Thanh Duy <duythanhpham2603@gmail.com>
2025-06-02 10:19:37 +0700

    chore: add gitignore

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
 
-const connectDatabase = require('./src/config/database')
-const route = require('./src/routes')
+const connectDatabase = require("./src/configs/database");
+const route = require("./src/routes");
 
-const app = express()
+const app = express();
 
 // apply middleware to read body data-request
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // use morgan for reading request log from clients
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 
 // configure cors
-app.use(cors())
+app.use(cors());
 
 // connect to MongoDB allats
-connectDatabase(app)
+connectDatabase(app);
 
 // routes navigate
-route(app)
-
+route(app);
